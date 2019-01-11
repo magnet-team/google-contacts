e7ab379cd8ecdf8d4e013d369b52af625edd61cc
sampatbadhe <sampat.badhe@kiprosh.com>
2019-01-11 13:35:14 +0530

    fix rubocop warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-$LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
+$LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require "google-contacts"
-require "pry"
+require 'google-contacts'
+require 'pry'
 
-Dir["./spec/support/*.rb"].each {|file| require file}
+Dir['./spec/support/*.rb'].each { |file| require file }
 
 RSpec.configure do |c|
   c.mock_with :rspec
